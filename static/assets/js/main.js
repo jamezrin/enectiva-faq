6723b1adb634a3f25cede2b15100937d61d1d4b6
Jaime Martínez Rincón <mrjaime1999@gmail.com>
2018-05-22 14:06:14 +0200

    Updated jquery, added jquery-autocomplete

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,58 +1,96 @@
-$(document).ready(function() {
-    var horseyList = horsey($("#search-by").get(0), {
-        suggestions: function (data, done) {
-            var currentRequest = $.ajax({
-                type: 'GET',
-                url: 'http://localhost:3000/',
-                contentType: 'text/plain',
-                data: {
-                    q: data,
-                    id: "lang-" + SITE_DATA.language,
-                    limit: 10
-                },
-                beforeSend: function() {
-                    if (currentRequest != null) {
-                        currentRequest.abort();
-                    }
-                },
-                success: function (data) {
-                    currentRequest = null;
-                    console.log('Response:', data);
-                    done(data);
-                },
-                error: function (err) {
-                    currentRequest = null;
-                    done([]);
-                }
-            });
-        },
+var currentXhr, language, searcher;
 
-        set: function(value) {
-            location.href = value.url;
-        },
+function init(language, searcher) {
+    this.language = language;
+    console.log("Language:", language);
 
-        render: function (li, suggestion) {
-            li.innerHTML = "<div>» " + suggestion.title + "</div>" +
-                "<div style=\"font-size: 12px\">" + (suggestion.matches[0].value || '') + "</div>";
+    this.searcher = searcher;
+    console.log("Search Server:", searcher);
+
+    $(document).ready(ready);
+}
+
+function ready() {
+    $('#search-by')
+        //When pressing enter in the search input
+        .on('keydown', function (e) {
+            if (e.which === 13) {
+                location.href = "/" + language + "/search?q=" + this.value;
+            }
+        })
+
+        //When clicking the 'x' or pressing enter
+        .on('search', function () {
+
+        })
+        .on('input', function () {
+            if (!this.value) {
+                //Clear search
+            }
+        });
+
+    //For the search page
+    var query = getParam('q');
+    if (query) displaySearch(query);
+}
+
+function getParam(param) {
+    var vars = {};
+    window.location.href.replace(location.hash, '').replace(
+        /[?&]+([^=&]+)=?([^&]*)?/gi,
+        function(m, key, value) {
+            vars[key] = value || '';
+        }
+    );
+
+    if (param) {
+        return vars[param] ? vars[param] : null;
+    }
+
+    return vars;
+}
+
+function displaySearch(query) {
+    search(query, function(data) {
+        console.log(data);
+
+        data.forEach(function (value) {
+            if (value.matches.length > 0) {
+                var result = document.createElement("div");
+                result.className = "search-result";
+
+                var anchor = document.createElement("a");
+                anchor.appendChild(document.createTextNode(value.title));
+                anchor.title = value.title;
+                anchor.href = value.url;
+                result.appendChild(anchor);
+
+                var description = document.createElement("p");
+                description.appendChild(document.createTextNode(value.matches[0].value));
+                result.appendChild(description);
+
+                document.getElementById('search-results').appendChild(result);
+            }
+        });
+    });
+}
+
+function search(query, callback) {
+    if (currentXhr)
+        currentXhr.abort();
+
+    currentXhr = $.ajax({
+        type: 'GET',
+        url: this.searcher,
+        contentType: 'text/plain',
+        data: {
+            q: query,
+            id: "lang-" + this.language,
+            limit: 10
+        }, success: function (data) {
+            callback(data)
+        }, error: function (err) {
+            console.log(err)
         }
     });
-
-    horseyList.refreshPosition();
-
-    $('#search-by').on('keydown', function (e) {
-        if (e.which === 13) {
-            console.log('Go to the search results page');
-            //Go to the search page
-            alert('call')
-        }
-    });
-
-    //When clicking the 'x' or pressing enter
-    $('input[type=search]').on('search', function () {
-        horseyList.clear();
-    }).on('input', function () {
-        if (!this.value) {
-            horseyList.clear();
-        }
-    });
-});
+}
